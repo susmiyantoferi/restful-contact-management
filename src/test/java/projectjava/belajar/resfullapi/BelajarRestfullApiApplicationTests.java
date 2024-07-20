08cd80430fc4821f46d3dd8f63ed83b7d7d3e45f
feri susmiyanto <irefmcr@gmail.com>
2024-07-20 16:40:23 +0700

    Create project & entity

---
+++ src/test/java/projectjava/belajar/resfullapi/BelajarRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package projectjava.belajar.resfullapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarRestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
